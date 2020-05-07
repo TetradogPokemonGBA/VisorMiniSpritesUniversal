fdc546708a5f808c89a3f7f466eefbb7395b8e03
Gabriel <tetradog@gmail.com>
2020-05-07 16:17:34 +0200

    ahora es universal :)

---
+++ MinisPaletaDiscover2/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gabriel.Cat.Wpf;
+using Microsoft.Win32;
+using PokemonGBAFramework.Core;
+using PokemonGBAFramework.Core.Extension;
+
+namespace MinisPaletaDiscover2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		public static readonly Bitmap Bmp1, Bmp2, Bmp3;
+		IList<MiniSpriteMapa> minis;
+		PaletasMinisMapa paletas;
+
+		static MainWindow()
+		{
+			ColorTable ct = new ColorTable();
+
+			Bmp1 = ct.ColorPicker.Imagen1;//.ToGbaBitmap();
+			Bmp2 = ct.ColorPicker.Imagen2;//.ToGbaBitmap();
+			Bmp3 = ct.ColorPicker.Imagen3;//.ToGbaBitmap();
+		}
+		public MainWindow()
+		{
+
+			InitializeComponent();
+			//CargarRom();
+		}
+
+		void CargarRom()
+		{
+			OpenFileDialog opn = new OpenFileDialog();
+			RomGba rom;
+
+			opn.Filter = "PokemonGBA|*.gba";
+			if (opn.ShowDialog().GetValueOrDefault())
+			{
+				Title = "Cargando";
+				rom = new RomGba(opn.FileName);
+				paletas = PaletasMinisMapa.Get(rom);
+
+				minis = MiniSpriteMapa.Get(rom,paletas);
+				stkMinis.Children.Clear();
+				stkPaletas.Children.Clear();
+
+				for (int i = 0; i < paletas.PaletasMinis.Count; i++)
+					AddPaleta(paletas.PaletasMinis[i]);
+				for (int i = 0; i < minis.Count; i++)
+					stkMinis.Children.Add(new MiniViwer(minis[i]));
+				Title = "Minis Paleta Discover";
+			}
+			else if (minis != null)
+			{
+				MessageBox.Show("No se ha cambiado la rom");
+			}
+			else
+			{
+				MessageBox.Show("No hay rom cargada...");
+			}
+		}
+
+		void AddPaleta(Paleta paleta)
+		{
+			Border border = new Border();
+			ColorTable ctPaleta = new ColorTable((System.Drawing.Color[])paleta.Colores.Clone());
+			ctPaleta.ColorPicker.Imagen1 = Bmp1;
+			ctPaleta.ColorPicker.Imagen2 = Bmp2;
+			ctPaleta.ColorPicker.Imagen3 = Bmp3;
+
+			ctPaleta.ColorChanged += CambiarPaletaMinis;
+			ctPaleta.MouseLeftButtonUp += PonPaletaATodos;
+			ctPaleta.Tag = paleta;
+			border.Child = ctPaleta;
+			border.BorderThickness = new Thickness(10);
+			stkPaletas.Children.Add(border);
+
+		}
+
+		void PonPaletaATodos(object sender, MouseButtonEventArgs e)
+		{
+			System.Drawing.Color[] paleta = ((ColorTable)sender).Colors;
+			Title = "Cargando";
+			for (int i = 0; i < stkMinis.Children.Count; i++)
+				((MiniViwer)stkMinis.Children[i]).SetPaleta(paleta);
+			Title = "Minis Paleta Discover";
+		}
+		void CambiarPaletaMinis(object sender, ColorChangedArgs e)
+		{
+			//los que la tengan se la ponen...
+			MiniViwer miniViwer;
+			ColorTable ct = (ColorTable)sender;
+			Paleta paletaOri = ct.Tag as Paleta;
+			System.Drawing.Color[] paleta = ct.Colors;
+			Title = "Cargando";
+			for (int i = 0; i < stkMinis.Children.Count; i++)
+			{
+				miniViwer = (MiniViwer)stkMinis.Children[i];
+				if (miniViwer.TienePaleta(paletaOri.Colores))
+					miniViwer.SetPaleta(paleta);
+			}
+			Title = "Minis Paleta Discover";
+		}
+		void BtnRestaurarPaleta_Click(object sender, RoutedEventArgs e)
+		{
+			Title = "Cargando";
+			for (int i = 0; i < stkMinis.Children.Count; i++)
+				((MiniViwer)stkMinis.Children[i]).RestaurarPaleta();
+			Title = "Minis Paleta Discover";
+		}
+		void BtnRestaurarPaletas_Click(object sender, RoutedEventArgs e)
+		{
+			Title = "Cargando";
+			for (int i = 0; i < paletas.PaletasMinis.Count; i++)
+				(((Border)stkPaletas.Children[i]).Child as ColorTable).Colors = paletas.PaletasMinis[i].Colores;
+			Title = "Minis Paleta Discover";
+		}
+		void MiCargar_Click(object sender, RoutedEventArgs e)
+		{
+			CargarRom();
+		}
+		void MiSobre_Click(object sender, RoutedEventArgs e)
+		{
+			if (MessageBox.Show("Autor:pikachu240\nLicencia:GNU GPL V3\nHecho para Sangus103\n¿Quieres ver el código fuente?", "Sobre la app", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+				System.Diagnostics.Process.Start("https://github.com/TetradogPokemonGBA/VisorMiniSpritesUniversal");
+		}
+	}
+}
---
+++ MinisPaletaDiscover2/MiniViwer.xaml.cs
@@ -0,0 +1,192 @@
+﻿using Gabriel.Cat.S.Extension;
+using Gabriel.Cat.S.Utilitats;
+using Microsoft.Win32;
+using PokemonGBAFramework.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinisPaletaDiscover2
+{
+    /// <summary>
+    /// Lógica de interacción para MiniViwer.xaml
+    /// </summary>
+    public partial class MiniViwer : UserControl
+    {
+		static bool isLight = true;
+		public const string EXTENSIONPALETA = ".pms";
+		MiniSpriteMapa mini;
+		public MiniViwer(MiniSpriteMapa mini)
+		{
+			if (mini == null)
+				throw new ArgumentNullException("mini");
+
+			ContextMenu menu = new ContextMenu();
+			MenuItem item = new MenuItem();
+			Image imgMini;
+
+			this.mini = mini;
+
+			item.Header = "Guardar Paleta";
+			item.Click += GuardarPaleta;
+			menu.Items.Add(item);
+
+			item = new MenuItem();
+			item.Header = "Cargar Paleta";
+			item.Click += CargarPaleta;
+			menu.Items.Add(item);
+
+			item = new MenuItem();
+			item.Header = "Restaurar Paleta";
+			item.Click += RestaurarPaleta;
+			menu.Items.Add(item);
+
+
+
+			InitializeComponent();
+			ContextMenu = menu;
+			ctPaleta.ColorPicker.Imagen1 = MainWindow.Bmp1;
+			ctPaleta.ColorPicker.Imagen2 = MainWindow.Bmp2;
+			ctPaleta.ColorPicker.Imagen3 = MainWindow.Bmp3;
+			ctPaleta.Colors = new System.Drawing.Color[Paleta.LENGTH];
+
+			for (int i = 0; i < mini.Minis.Count; i++)
+			{
+				imgMini = new Image();
+				menu = new ContextMenu();
+				item = new MenuItem();
+				item.Header = "Guardar Mini";
+				item.Click += GuardarMini;
+				item.Tag = imgMini;
+				menu.Items.Add(item);
+
+				item = new MenuItem();
+				item.Header = "Copy offset";
+				item.Click += (s, e) => CopiarOffset(s as MenuItem);
+				item.Tag = i;
+				menu.Items.Add(item);
+
+				imgMini.ContextMenu = menu;
+				imgMini.SetImage(mini.Minis[i].GetBitmap(mini.Paleta));
+				ugMinis.Children.Add(imgMini);
+			}
+
+			RestaurarPaleta();
+			if (!isLight)
+			{
+				Background = Brushes.LightBlue;
+
+			}
+			else
+			{
+				Background = Brushes.White;
+			}
+			isLight = !isLight;
+		}
+
+		void CopiarOffset(MenuItem miMini)
+		{
+			Clipboard.SetText((Hex)mini.Minis[(int)miMini.Tag].Offset);
+		}
+		void GuardarMini(object sender, RoutedEventArgs e)
+		{
+			Image img = (sender as MenuItem).Tag as Image;
+			SaveFileDialog sfdImgMini = new SaveFileDialog();
+
+			if (img != null && sfdImgMini.ShowDialog().GetValueOrDefault())
+			{
+				try
+				{
+					img.ToBitmap().Save(sfdImgMini.FileName + ".png", System.Drawing.Imaging.ImageFormat.Png);
+				}
+				catch
+				{
+					MessageBox.Show("Ha ocurrido un problema al exportar la imagen del mini", "No se ha podido hacer...", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+			}
+		}
+
+		public bool TienePaleta(System.Drawing.Color[] paleta)
+		{
+			bool tienePaleta = true;
+			for (int i = 0; i < paleta.Length && tienePaleta; i++)
+				tienePaleta = paleta[i].Equals(mini.Paleta.Colores[i]);
+
+			return tienePaleta;
+		}
+
+		public void RestaurarPaleta(object sender = null, RoutedEventArgs e = null)
+		{
+			SetPaleta(mini.Paleta.Colores);
+		}
+		public void SetPaleta(System.Drawing.Color[] paleta)
+		{
+			for (int i = 0; i < paleta.Length; i++)
+				ctPaleta.Colors[i] = paleta[i];
+			ctPaleta.Colors = ctPaleta.Colors;
+			CambioPaletaMinis();
+		}
+		void CargarPaleta(object sender = null, RoutedEventArgs e = null)
+		{
+			const int BYTESCOLOR = 4;
+			OpenFileDialog opn = new OpenFileDialog();
+			BinaryReader br = null;
+			opn.Filter = "PaletaMiniSprite|*." + EXTENSIONPALETA + "|TODOS|*.*";
+			if (opn.ShowDialog().GetValueOrDefault())
+			{
+				try
+				{
+					br = new BinaryReader(new FileStream(opn.FileName, FileMode.Open));
+					for (int i = 0; i < ctPaleta.Colors.Length; i++)
+						ctPaleta.Colors[i] = Serializar.ToColor(br.ReadBytes(BYTESCOLOR));
+				}
+				finally
+				{
+					br.Close();
+
+				}
+				CambioPaletaMinis();
+			}
+		}
+		void GuardarPaleta(object sender, RoutedEventArgs e)
+		{
+			SaveFileDialog sfdPaleta = new SaveFileDialog();
+			BinaryWriter bw;
+			if (sfdPaleta.ShowDialog().GetValueOrDefault())
+			{
+				if (File.Exists(sfdPaleta.FileName))
+					File.Delete(sfdPaleta.FileName);
+
+				bw = new BinaryWriter(new FileStream(sfdPaleta.FileName + EXTENSIONPALETA, FileMode.Create));
+				for (int i = 0; i < ctPaleta.Colors.Length; i++)
+					bw.Write(Serializar.GetBytes(ctPaleta.Colors[i]));
+				bw.Close();
+
+
+			}
+		}
+
+		void CtPaleta_ColorChanged(object sender, Gabriel.Cat.Wpf.ColorChangedArgs e)
+		{
+			CambioPaletaMinis();
+		}
+		void CambioPaletaMinis()
+		{
+			Paleta paleta = new Paleta(ctPaleta.Colors);
+			//actualizo los minis
+			for (int i = 0; i < mini.Minis.Count; i++)
+				((Image)ugMinis.Children[i]).SetImage(mini.Minis[i].GetBitmap(paleta));
+
+		}
+	}
+}
